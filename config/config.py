a487ea8c97d779730a20f6b39aab2a5911281ab2
Team Freedom <167733685+fps90@users.noreply.github.com>
2024-12-14 03:54:40 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -88,7 +88,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TheTeamVivek/YukkiMusic",
+    "https://github.com/fps90/muntazer",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
